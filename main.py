e201739fbeefa1540da4271f6fea00af47523965
ilya138 <ilya138@macbook.com>
2019-10-13 08:14:40 +0300

    minor fixes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random, math, pygame
+import random, math, pygame, sys, datetime
 from pygame.locals import *
 
 # constants
@@ -16,75 +16,78 @@ BIRD_JUMP_VELOCITY = 5
 
 class PipeLine():
 
-    rect_Top_Pipe = None
-    rect_Bottom_Pipe = None 
+    topPipeSprite = None
+    bottomPipeSprite = None 
     x = None
 
     def tick(self, scene):
         
-        pygame.draw.rect(scene, COLOR_BLACK, self.rect_Top_Pipe)
-        pygame.draw.rect(scene, COLOR_BLACK, self.rect_Bottom_Pipe)
+        pygame.draw.rect(scene, COLOR_BLACK, self.topPipeSprite)
+        pygame.draw.rect(scene, COLOR_BLACK, self.bottomPipeSprite)
 
-        self.rect_Top_Pipe.move_ip(-PIPE_SPEED, 0)
-        self.rect_Bottom_Pipe.move_ip(-PIPE_SPEED, 0)
+        self.topPipeSprite.move_ip(-PIPE_SPEED, 0)
+        self.bottomPipeSprite.move_ip(-PIPE_SPEED, 0)
 
-        pygame.draw.rect(scene, COLOR_WHITE, self.rect_Top_Pipe)
-        pygame.draw.rect(scene, COLOR_WHITE, self.rect_Bottom_Pipe)
+        pygame.draw.rect(scene, COLOR_WHITE, self.topPipeSprite)
+        pygame.draw.rect(scene, COLOR_WHITE, self.bottomPipeSprite)
 
-        self.x = self.rect_Top_Pipe.x
+        self.x = self.topPipeSprite.x
 
     def __init__(self, scene):
 
         self.gap_pos_y = random.randint(50, WINSIZE[1] - 120)
         self.gap_size = random.randint(80, 120)
         
-        topPipePos = (WINSIZE[0] - PIPE_WIDTH, 0, PIPE_WIDTH, self.gap_pos_y)
-        bottomPipePos = (WINSIZE[0] - PIPE_WIDTH, self.gap_pos_y + self.gap_size, PIPE_WIDTH, WINSIZE[1] - self.gap_pos_y + self.gap_size)
-        self.rect_Top_Pipe = pygame.draw.rect(scene, COLOR_WHITE, topPipePos)
-        self.rect_Bottom_Pipe = pygame.draw.rect(scene, COLOR_BLACK, bottomPipePos)
-        self.x = self.rect_Top_Pipe.x
+        topPipeSpritePos = (WINSIZE[0] - PIPE_WIDTH, 0, PIPE_WIDTH, self.gap_pos_y)
+        bottomPipeSpritePos = (WINSIZE[0] - PIPE_WIDTH, self.gap_pos_y + self.gap_size, PIPE_WIDTH, WINSIZE[1] - self.gap_pos_y + self.gap_size)
+        self.topPipeSprite = pygame.draw.rect(scene, COLOR_WHITE, topPipeSpritePos)
+        self.bottomPipeSprite = pygame.draw.rect(scene, COLOR_BLACK, bottomPipeSpritePos)
+        self.x = self.topPipeSprite.x
         
-
 class Bird():
 
-    rect = None
+    sprite = None
     velocity = 0
     score = 0
     dead = False
+    color = None
+
+    def __init__(self, scene, color=COLOR_WHITE):
+        self.sprite = pygame.draw.rect(scene, color, (BIRD_START_X, BIRD_START_Y, BIRD_SIZE, BIRD_SIZE))
+        self.created = datetime.datetime.now()
 
     def tick(self, scene):
 
         if self.dead:
             return
 
-        y = int(self.rect.y - self.velocity)
+        y = int(self.sprite.y - self.velocity)
 
         if y <= 0 or y >= WINSIZE[1]:
             self.kill()
         else:
             self.draw(scene, COLOR_BLACK)
-            self.rect.move_ip(0, -self.velocity)
+
+            self.sprite.move_ip(0, -self.velocity)
             self.velocity = self.velocity - GRAVITY
             self.draw(scene, COLOR_WHITE)
 
     def draw(self, scene, color):
 
         global font
-        pygame.draw.rect(scene, color, self.rect)
+        pygame.draw.rect(scene, color, self.sprite)
         score_text = font.render("{}".format(self.score), True, COLOR_BLACK)
-        scene.blit(score_text, self.rect)
+        scene.blit(score_text, self.sprite)
 
     def jump(self):
         self.velocity = BIRD_JUMP_VELOCITY
 
     def overlaps(self, pipes):
-        return pipes.rect_Top_Pipe.colliderect(self.rect) or pipes.rect_Bottom_Pipe.colliderect(self.rect)
-
-    def __init__(self, scene):
-        self.rect = pygame.draw.rect(scene, COLOR_WHITE, (BIRD_START_X, BIRD_START_Y, BIRD_SIZE, BIRD_SIZE))
+        return pipes.topPipeSprite.colliderect(self.sprite) or pipes.bottomPipeSprite.colliderect(self.sprite)
 
     def kill(self):
         self.dead = True
+        self.lifeTime = (datetime.datetime.now() - self.created).microseconds
 
 def main():
 
@@ -109,8 +112,11 @@ def main():
         # Controls and stuff
         pygame.display.update()
         for e in pygame.event.get():
-            if hasattr(e, "key"):
-                if e.key == K_UP:
+            if e.type == KEYDOWN:
+                if e.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit(0)
+                elif e.key == K_UP:
                     for bird in birds:
                         bird.jump()
                 elif e.key == K_SPACE and not game_started:
@@ -118,9 +124,6 @@ def main():
                     birds = [Bird(scene)]
                     game_started = True
                     scene.fill(COLOR_BLACK)
-            elif e.type == MOUSEBUTTONDOWN:
-                for bird in birds:
-                    bird.jump()  
 
         if not game_started:
             text = font.render("Press SPACE to start the game", True, COLOR_WHITE)
@@ -135,18 +138,17 @@ def main():
                     if bird.overlaps(pipeLine):
                         bird.kill()
 
-                if bird.rect.x >= pipeLine.rect_Top_Pipe.x and len(pipeLines) == 1:
+                if bird.sprite.x >= pipeLine.x and len(pipeLines) == 1:
                     pipeLines.append(PipeLine(scene))
                     bird.score += 1
 
         if all(bird.dead for bird in birds):
             game_started = False
 
-        for lineToDelete in [pipe for pipe in pipeLines if pipe.x < -PIPE_WIDTH]:
-            pipeLines.remove(lineToDelete)
+        for pipeToDelete in [pipe for pipe in pipeLines if pipe.x < -PIPE_WIDTH]:
+            pipeLines.remove(pipeToDelete)
 
         clock.tick(60)
 
-# if python says run, then we should run
 if __name__ == "__main__":
     main()
