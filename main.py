c22b821b5ba295580c4731835f4d712c807cd87d
Ilya <zakharov138@gmail.com>
2019-10-13 19:26:30 +0300

    sprite changes + minor gameplay change

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
+# Flappy bird game using pygame + self-taught algorithm
+#
+# Ilya Zakharov, 2019
+
 import random, math, pygame, sys, datetime
 from pygame.locals import *
 
 # constants
 WINSIZE = [640, 480]
 WINCENTER = [320, 240]
+GRAVITY = 0.98
+
+# Colors
 COLOR_WHITE = 255, 255, 255
 COLOR_BLACK = 0, 0, 0
 COLOR_BACKGROUND = 107, 140, 255
-PIPE_WIDTH = 50
-PIPE_SPEED = 3
-GRAVITY = 0.98
+
+# Pipes
+PIPE_WIDTH = 100
+PIPE_START_SPEED = 3
+PIPE_GAP_MIN = 100
+PIPE_GAP_MAX = 150
+PIPE_HEADER_HEIGHT = 42
+
+# Player
 PLAYER_START_X = 100
 PLAYER_START_Y = 50
-PLAYER_SIZE = 20
+PLAYER_SIZE = 40
 PLAYER_JUMP_VELOCITY = 5
 
-PLAYER_SPRITE = pygame.image.load('res/player.png')
-PLAYER_SPRITE = pygame.transform.scale(PLAYER_SPRITE, (PLAYER_SIZE, PLAYER_SIZE))
-PIPE_SPRITE = pygame.image.load('res/pipe.png')
+# Sprites
+PLAYER_SPRITE = pygame.transform.scale(pygame.image.load('res/player.png'), (PLAYER_SIZE, PLAYER_SIZE))
+PIPE_BODY_SPRITE = pygame.image.load('res/pipe.png')
+PIPE_BOTTOM_SPRITE = pygame.transform.scale(pygame.image.load('res/pipeTop.png'), (PIPE_WIDTH, PIPE_HEADER_HEIGHT))
+PIPE_TOP_SPRITE = pygame.transform.flip(PIPE_BOTTOM_SPRITE, False, True)
 
+# Global variables
+pipeSpeed = PIPE_START_SPEED
 class PipeLine():
 
     topPipeSprite = None
@@ -27,34 +44,55 @@ class PipeLine():
 
     def tick(self, scene):
         
+        # workaround of bug when rect created outside the screen has negative width
+        if self.topPipeSprite.width <= 0:
+            self.topPipeSprite.width = PIPE_WIDTH
+        if self.bottomPipeSprite.width <= 0:
+            self.bottomPipeSprite.width = PIPE_WIDTH
+
         self.clear(scene)
-        self.topPipeSprite.move_ip(-PIPE_SPEED, 0)
-        self.bottomPipeSprite.move_ip(-PIPE_SPEED, 0)
+        self.topPipeSprite.move_ip(-pipeSpeed, 0)
+        self.bottomPipeSprite.move_ip(-pipeSpeed, 0)
         self.draw(scene)
 
         self.x = self.topPipeSprite.x
 
     def clear(self, scene):
-            pygame.draw.rect(scene, COLOR_BACKGROUND, self.topPipeSprite)
-            pygame.draw.rect(scene, COLOR_BACKGROUND, self.bottomPipeSprite)
+            pygame.draw.rect(scene, COLOR_BACKGROUND, (self.x, 0, PIPE_WIDTH, WINSIZE[1]))
             
     def draw(self, scene):
-        topPipeSprite = pygame.transform.flip(PIPE_SPRITE, False, True)
-        topPipeSprite = pygame.transform.scale(topPipeSprite, (PIPE_WIDTH, self.topPipeSprite.h))
-        bottomPipeSprite = pygame.transform.scale(PIPE_SPRITE, (PIPE_WIDTH, self.bottomPipeSprite.h))
+
+        # Top pipe
+        topPipeSprite = pygame.transform.scale(PIPE_BODY_SPRITE, (PIPE_WIDTH, self.topPipeSprite.h))
         scene.blit(topPipeSprite, self.topPipeSprite.topleft)
+        scene.blit(PIPE_TOP_SPRITE, (self.topPipeSprite.bottomleft[0], self.topPipeSprite.bottomleft[1] - PIPE_HEADER_HEIGHT))
+
+        # Bottom pipe
+        bottomPipeSprite = pygame.transform.scale(PIPE_BODY_SPRITE, (PIPE_WIDTH, self.bottomPipeSprite.h))
         scene.blit(bottomPipeSprite, self.bottomPipeSprite.topleft)
+        scene.blit(PIPE_BOTTOM_SPRITE, self.bottomPipeSprite.topleft)
 
     def __init__(self, scene):
 
-        self.gap_pos_y = random.randint(50, WINSIZE[1] - 120)
-        self.gap_size = random.randint(80, 120)
+        self.gap_pos_y = random.randint(50, WINSIZE[1] - PIPE_GAP_MAX)
+        self.gap_size = random.randint(PIPE_GAP_MIN, PIPE_GAP_MAX)
         
-        topPipeSpritePos = (WINSIZE[0] - PIPE_WIDTH, 0, PIPE_WIDTH, self.gap_pos_y)
-        bottomPipeSpritePos = (WINSIZE[0] - PIPE_WIDTH, self.gap_pos_y + self.gap_size, PIPE_WIDTH, WINSIZE[1] - self.gap_pos_y + self.gap_size)
-        self.topPipeSprite = pygame.draw.rect(scene, COLOR_WHITE, topPipeSpritePos)
-        self.bottomPipeSprite = pygame.draw.rect(scene, COLOR_BLACK, bottomPipeSpritePos)
+        topPipeSpritePos = self.getpos(x = WINSIZE[0] + PIPE_WIDTH, 
+                                       y = 0, 
+                                       width = PIPE_WIDTH,
+                                       height = self.gap_pos_y)
+
+        bottomPipeSpritePos = self.getpos(x = WINSIZE[0] + PIPE_WIDTH,
+                                          y = self.gap_pos_y + self.gap_size,
+                                          width = PIPE_WIDTH,
+                                          height = WINSIZE[1] - self.gap_pos_y + self.gap_size)
+
+        self.topPipeSprite = pygame.draw.rect(scene, COLOR_BACKGROUND, topPipeSpritePos)
+        self.bottomPipeSprite = pygame.draw.rect(scene, COLOR_BACKGROUND, bottomPipeSpritePos)
         self.x = self.topPipeSprite.x
+
+    def getpos(self, x, y, width, height):
+        return (x, y, width, height)
         
 class Player():
 
@@ -101,7 +139,7 @@ class Player():
 
 def main():
 
-    global font
+    global font, pipeSpeed
 
     # initialize and prepare scene
     random.seed()
@@ -140,9 +178,6 @@ def main():
             scene.blit(text, (WINSIZE[0] // 3, WINSIZE[1] // 2))
         else:    
             for player in players:
-                
-                player.tick(scene)
-
                 for pipeLine in pipeLines:
                     pipeLine.tick(scene)
 
@@ -152,10 +187,15 @@ def main():
                 if player.sprite.x >= pipeLine.x and len(pipeLines) == 1:
                     pipeLines.append(PipeLine(scene))
                     player.score += 1
+                            
+                player.tick(scene)
 
             maxScore = max(player.score for player in players)
             text = font.render("Score: {}".format(maxScore), True, COLOR_WHITE)
             scene.blit(text, (50, 20))
+
+            # Speed slightly increases every 5 pipes
+            pipeSpeed = PIPE_START_SPEED + maxScore // 5
 
         if all(player.dead for player in players):
             game_started = False
